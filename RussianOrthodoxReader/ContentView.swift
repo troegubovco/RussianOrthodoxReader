33d3d3b37cc5bfb73c6b30e19aa6e42dc2ca5343
Andrey <andreytroegubov@gmail.com>
2026-02-25 23:53:13 -0500

    Initial Commit

---
+++ RussianOrthodoxReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RussianOrthodoxReader
+//
+//  Created by Andrey Troegubov on 2/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RussianOrthodoxReader/RussianOrthodoxReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RussianOrthodoxReaderApp.swift
+//  RussianOrthodoxReader
+//
+//  Created by Andrey Troegubov on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct RussianOrthodoxReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
